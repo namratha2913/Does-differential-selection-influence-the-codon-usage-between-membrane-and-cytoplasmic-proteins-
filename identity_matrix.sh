c6b9fe9b8cac4b667e66e3262d2ef05187f142b3
Namratha <namratha.mraj@gmail.com>
2025-06-17 21:20:43 +0530

    Delete identity_matrix.sh

--- identity_matrix.sh
+++
@@ -1,8 +0,0 @@
-for file in /home/namratha/Namratha/CUB_codon_usage_bias/1-3-24Analysis/Results_all/alignment_cyt/*; do
-
-	echo $file
-	echo $(basename -a $file)
-	#echo $pseudo_file
-	pseqsid -i $file
-
-done
